5899aee817ee3c9ab4eaa991de85fd1b2c87a97a
udhaya <udhaya3011@gmail.com>
2022-02-02 10:59:28 +0530

    scrolling navbar fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,9 +63,28 @@ navToggler.addEventListener("click", () => {
 
     // alert("hello");
 
-    navIcon.forEach(Icon =>{
+    navIcon.forEach(Icon => {
         Icon.classList.toggle("hidden")
     })
 
 });
+
+function navbarfixed() {
+    const header_dom = document.querySelector(".header");
+
+    const nav_offset_top = header_dom.clientHeight + 50;
+
+    window.addEventListener("scroll", () => {
+
+        let scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scroll >= nav_offset_top) {
+            header_dom.classList.add("navbar-fixed");
+        } else{
+            header_dom.classList.remove("navbar-fixed");
+        }
+    });
+}
+
+navbarfixed();
 
